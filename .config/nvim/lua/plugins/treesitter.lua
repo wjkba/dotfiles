93b919e9d8f8377fab77a8563e4f06038c25811c
wjkba <63542318+wjkba@users.noreply.github.com>
2025-07-12 12:44:19 +0200

    add lsp

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,20 +2,20 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ":TSUpdate",
     config = function()
-	local configs = require("nvim-treesitter.configs")
-	configs.setup({
-	    highlight = { enable = true},
-	    indent = {enable = true},
-	    autotage = {enable = true},
-	    ensure_installed = {
-		"lua",
-		"tsx",
-		"typescript",
-		"javascript",
-		"python",
-		"markdown",
-	    },
-	    auto_install = false,
-	})
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            highlight = { enable = true },
+            indent = { enable = true },
+            autotage = { enable = true },
+            ensure_installed = {
+                "lua",
+                "tsx",
+                "typescript",
+                "javascript",
+                "python",
+                "markdown",
+            },
+            auto_install = false,
+        })
     end
 }
