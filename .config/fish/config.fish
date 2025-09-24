5de2993d63e10f5c8e82109e65254c12c5ca2488
wjkba <63542318+wjkba@users.noreply.github.com>
2025-09-24 13:21:49 +0200

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,11 @@ end
 
 set -g fish_greeting ""
 
+alias zj='zellij'
+
 zoxide init fish | source
+
+set -x EDGE_PATH "/usr/bin/ungoogled-chromium"
+set -x ANDROID_HOME $HOME/Android/Sdk
+set -x PATH $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools $PATH
+
